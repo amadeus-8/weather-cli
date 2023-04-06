e60c650219c4a9bf0c9d57c404752c349d9245d3
Alikhan <f.good301@gmail.com>
2023-04-06 21:55:01 +0600

    Added process.env

--- services/api.service.js
+++ services/api.service.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
-import https from 'https';
+// import https from 'https';
 import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
 
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather'
 
 const getWeather = async (city) => {
-	const token = await getKeyValue(TOKEN_DICTIONARY.token);
+	const token = process.env.TOKEN ?? await getKeyValue(TOKEN_DICTIONARY.token);
 	
 	if(!token) {
 		throw new Error('No api key set, set it with comman -t [API_KEY]')
@@ -22,23 +22,7 @@ const getWeather = async (city) => {
 	
 	console.log(data);
 	
-	
-	// const url = new URL('https://api.openweathermap.org/data/2.5/weather')
-	// url.searchParams.append('q', city);
-	// url.searchParams.append('appid', token);
-	// url.searchParams.append('lang', 'ru');
-	// url.searchParams.append('units', 'metric');
-	
-	// https.get(url, (response) => {
-		// let res = '';
-		// response.on('data', (chunk) => {
-			// res += chunk;
-		// });
-		
-		// response.on('end', () => {
-			// console.log(res)
-		// });
-	// });
+	return data;
 };
 
 export {
